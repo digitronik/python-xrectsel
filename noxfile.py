b3fd28f7076225a528091ade046de2d318329388
Nikhil Dhandre <nik.digitronik@live.com>
2020-04-26 13:08:35 +0530

    Added Unit tests; remove tox and introduce nox with pre-commit checks

---
+++ noxfile.py
@@ -0,0 +1,48 @@
+import nox
+
+nox.options.sessions = ["pre_commit", "tests", "coverage"]
+
+
+@nox.session
+def pre_commit(session):
+    """pre-commit checks"""
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "-a")
+
+
+@nox.session(python=["3.5", "3.6", "3.7", "3.8"])
+def tests(session):
+    """Run unit test over different python env with code coverage"""
+    session.install("pytest", "pytest-cov", "-e", ".")
+    session.run(
+        "py.test",
+        "--cov=xrectsel",
+        "--cov-report=xml",
+        "--cov-branch",
+        "--color=yes",
+        "-s",
+        "-v",
+    )
+
+
+@nox.session()
+def coverage(session):
+    """This is only to run test on ci"""
+    session.install("coverage")
+    session.run("coverage", "report")
+
+
+@nox.session()
+def package(session):
+    """Build and verify package"""
+    session.install("twine", "setuptools", "wheel")
+    session.run("python", "setup.py", "sdist", "bdist_wheel")
+    session.run("ls", "-l", "dist")
+    session.run("python", "-m", "twine", "check", "dist/*")
+
+
+@nox.session()
+def dev_setup(session):
+    """Ensure development environment works everywhere. mainly with ci on different platform"""
+    session.run("python", "-m", "pip", "install", "-e", ".[dev]")
+    session.run("python", "-c", "from xrectsel import XRectSel; XRectSele()")
